dd70d03eb115eacca90590a212d258266144267e
missgoon <missgoon@163.com>
2016-03-21 06:09:11 +0800

    update

--- base_func.py
+++ base_func.py
@@ -4,14 +4,13 @@
 import requests
 import os
 
-def get_pic(save_path,iamge_url):
+def get_pic(save_path,image_url):
   '''
     根据给定的地址下载并保存到本地文件中
   '''
-  image_url="http://static.yidianzixun.com/beauty/imgs/i_000N0FBg.jpg"
-  save_path="i_000N0FBg.jpg"
   if len(save_path)==0 or len(image_url)==0: return False
-  if not os.path.exists(r"/root/get_beauty/%s"%save_path): print("creating a file %s\n"%save_path);os.system(r"touch %s"%save_path)
+  if os.path.exists(r"/root/pictures/%s"%save_path): return False
+  #if not os.path.exists(r"/root/get_beauty/%s"%save_path): print("creating a file %s\n"%save_path);os.system(r"touch %s"%save_path)
   response=requests.get(image_url,stream=True)
   image=response.content
   print("begining save image %s...\n"%save_path)
@@ -28,10 +27,14 @@ def get_pic(save_path,iamge_url):
 def get_url(num):
   '''
     从根据得到的num，访问网址http://www.yidianzixun.com/api/q/?path=channel|news-list-for-channel&channel_id=u241&fields=up&cstart=1&cend=2
+    http://www.yidianzixun.com/api/q/?path=channel|news-list-for-keyword&display=美女&word_type=token&fields=docid&fields=category&fields=date&fields=image&fields=image_urls&fields=like&fields=source&fields=title&fields=url&fields=comment_count&fields=summary&fields=up&cstart=10&cend=20&version=999999&infinite=true
     并从中得到图片的名称和地址
   '''
-  url="http://www.yidianzixun.com/api/q/?path=channel|news-list-for-channel&channel_id=u241&fields=up&cstart=%d&cend=%d"%(num,num+1)
+  #url="http://www.yidianzixun.com/api/q/?path=channel|news-list-for-channel&channel_id=u241&fields=up&cstart=%d&cend=%d"%(num,num+1)
+  url="http://www.yidianzixun.com/api/q/?path=channel|news-list-for-keyword&display=美女&word_type=token&fields=docid&fields=category&fields=date&fields=image&fields=image_urls&fields=like&fields=source&fields=title&fields=url&fields=comment_count&fields=summary&fields=up&cstart=%d&cend=%d&version=999999&infinite=true"%(num,num+1)
   response=requests.get(url)
-  a_str=(response.content).split('"channel_image":"')
-  image_url="".join(a[1].strip("\}").strip('\"').split("\\"))
-  print(image_url)
+  a_str=(response.content).split('"image":"')[1].split('"')
+  image_url="".join(a_str[0].strip("\}").strip('\"').split("\\"))
+  name=image_url.split("/")[-1]
+  print("get image info name: %s url:%s"%(name,image_url))
+  return [name,image_url]
--- main.py
+++ main.py
@@ -5,4 +5,13 @@ from sys import path
 path.append("/root/get_beauty")
 import base_func
 
-base_func.get_pic("","")
+count=0
+while count<10000:
+  try:
+    image_info=base_func.get_url(count)
+    base_func.get_pic("/root/pictures/%s"%image_info[0],image_info[1])
+  except:
+  	pass
+  finally:
+    count+=1  
+
