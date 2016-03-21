a6543da144b120f45e838e1d947ceb9fe552f397
wangyang <missgoon@163.com>
2016-03-21 14:34:00 +0800

    updat

--- main.py
+++ main.py
@@ -7,14 +7,13 @@ import base_func
 import redis
 import time
 
-count=1
 db=redis.StrictRedis(host="139.129.45.40",port=6379,db=0)
-while count!=0:
+while True:
   try:
-    image_info=base_func.get_url(count)
+    image_info=base_func.get_url(db.incr("curr_cursor"))
     if db.get(image_info[0])==-1: base_func.get_pic("/root/pictures/%s"%image_info[0],image_info[1])
   except:
   	pass
   finally:
-    count+=1  
+  	pass
 
