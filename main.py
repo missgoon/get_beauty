d43a21d151178c5ae4ab609cf7476d295b341843
wangyang <missgoon@163.com>
2016-03-21 14:12:38 +0800

    update

--- main.py
+++ main.py
@@ -7,15 +7,14 @@ import base_func
 import redis
 import time
 
-count=0
+count=1
 db=redis.StrictRedis(host="139.129.45.40",port=6379,db=0)
-while count<10000:
+while count!=0:
   try:
     image_info=base_func.get_url(count)
-    if db.get(image_info[0]): base_func.get_pic("/root/pictures/%s"%image_info[0],image_info[1])
+    if db.get(image_info[0])==-1: base_func.get_pic("/root/pictures/%s"%image_info[0],image_info[1])
   except:
   	pass
   finally:
     count+=1  
-    time.sleep(5)
 
